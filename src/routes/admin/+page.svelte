1bd6a84afbddfff701d0208341f0029cf3d0390a
Jason Phan <jsonphan@gmail.com>
2025-05-07 16:19:04 -0700

    Added simple debounce search.

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,6 +1,34 @@
 <script>
   export let data;
   let pokemonList = data.pokemonList;
+  let searchName = "";
+  let result = null;
+  let timeout;
+
+  async function getPokemon(name) {
+    try {
+      const pokeInfo = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`
+      );
+      const data = await pokeInfo.json();
+      result = {
+        name: data.name,
+        image: data.sprites.front_default,
+      };
+    } catch (rror) {
+      result = null;
+    }
+  }
+
+  function getInput(event) {
+    clearTimeout(timeout);
+    const userInput = event.target.value;
+    searchName = userInput;
+
+    timeout = setTimeout(() => {
+      getPokemon(searchName);
+    }, 400);
+  }
 </script>
 
 <nav>
@@ -9,15 +37,21 @@
 
 <h1>PokeStore</h1>
 
-<input type="text" placeholder="Search Pokemon..." />
+<input type="text" placeholder="Search Pokemon..." on:input={getInput} />
 
-<div class="grid">
-  <ul>
-    {#each pokemonList as pokemon}
-      <li><img src={pokemon.image} alt={pokemon.name} /></li>
-    {/each}
-  </ul>
-</div>
+{#if result}
+  <div class="grid">
+    <img src={result.image} alt={result.name} />
+  </div>
+{:else}
+  <div class="grid">
+    <ul>
+      {#each pokemonList as pokemon}
+        <li><img src={pokemon.image} alt={pokemon.name} /></li>
+      {/each}
+    </ul>
+  </div>
+{/if}
 
 <style>
   nav {
@@ -33,11 +67,13 @@
     display: block;
     margin: 0 auto;
     justify-content: center;
+    width: 200px;
   }
 
   .grid {
     display: flex;
     justify-content: center;
+    padding: 2 rem;
   }
   ul {
     display: grid;
@@ -45,5 +81,13 @@
     list-style: none;
     padding: 0;
     gap: 1rem;
+  }
+
+  li {
+    border-radius: 10px;
+  }
+  img {
+    width: 100px;
+    height: auto;
   }
 </style>
